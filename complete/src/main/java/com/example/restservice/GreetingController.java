f88a38328e9156c4ecaa1f0abfe5a4863d06dc70
sahithdev9 <sahith.dev9@gmail.com>
2026-07-23 16:39:53 +0530

    Update GreetingController.java
    Signed-off-by: sahithdev9 <sahith.dev9@gmail.com>

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -3,17 +3,16 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, Team!";
+    private final AtomicLong counter = new AtomicLong();
 
-  @GetMapping("/greeting")
-  public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
-    return new Greeting(counter.incrementAndGet(), template.formatted(name));
-  }
+    @GetMapping("/greeting")
+    public Greeting greeting() {
+        return new Greeting(counter.incrementAndGet(), template);
+    }
 }
