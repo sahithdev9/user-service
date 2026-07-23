7ec736946a373d7a751fa0aa3098e7b6db0e9ed9
sahithdev9 <sahith.dev9@gmail.com>
2026-07-23 12:25:08 +0530

    Update install.sh
    Signed-off-by: sahithdev9 <sahith.dev9@gmail.com>

--- complete/scripts/install.sh
+++ complete/scripts/install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
 set -e
 
 echo "===== Install Phase Started ====="
 
-APP_DIR=/home/ec2-user/user-service
+sudo mkdir -p /home/ec2-user/user-service
 
-# Create application directory if it doesn't exist
-mkdir -p $APP_DIR
+JAR=$(find target -name "*.jar" | head -1)
 
-# Copy the JAR file from the deployment bundle
-cp *.jar $APP_DIR/app.jar
-
-echo "JAR copied successfully."
+sudo cp "$JAR" /home/ec2-user/user-service/app.jar
 
 echo "===== Install Phase Completed ====="
