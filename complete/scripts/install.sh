6ff8a5997acd21aafa5af48e2f300108fc3fd238
sahithdev9 <sahith.dev9@gmail.com>
2026-07-23 12:35:09 +0530

    Update install.sh
    Signed-off-by: sahithdev9 <sahith.dev9@gmail.com>

--- complete/scripts/install.sh
+++ complete/scripts/install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-
 set -e
 
 echo "===== Install Phase Started ====="
 
 APP_DIR=/home/ec2-user/user-service
-
 mkdir -p "$APP_DIR"
 
-JAR=$(find target -name "*.jar" | head -1)
+DEPLOY_DIR="$PWD"
+
+echo "Current directory: $DEPLOY_DIR"
+
+JAR=$(find "$DEPLOY_DIR" -type f -name "*.jar" | head -1)
+
+if [ -z "$JAR" ]; then
+    echo "ERROR: No JAR file found!"
+    find "$DEPLOY_DIR" -type f
+    exit 1
+fi
+
+echo "Found JAR: $JAR"
 
 cp "$JAR" "$APP_DIR/app.jar"
-
-echo "JAR copied successfully."
 
 echo "===== Install Phase Completed ====="
