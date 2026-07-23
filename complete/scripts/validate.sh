b3cc5a3764bebfdf457bb15aaefde97ffb8e985a
sahithdev9 <sahith.dev9@gmail.com>
2026-07-23 10:57:49 +0530

    Update validate.sh
    Signed-off-by: sahithdev9 <sahith.dev9@gmail.com>

--- complete/scripts/validate.sh
+++ complete/scripts/validate.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+echo "===== Validating Application ====="
+
+sleep 15
+
+PID=$(pgrep -f app.jar)
+
+if [ -z "$PID" ]; then
+    echo "Application failed to start."
+    exit 1
+else
+    echo "Application is running with PID: $PID"
+fi
+
+echo "===== Validation Successful ====="
+
+exit 0
