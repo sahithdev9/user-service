a1cf976b1782c82a77aa25478904fd2bd8fa65a3
sahithdev9 <sahith.dev9@gmail.com>
2026-07-23 10:56:18 +0530

    Update start.sh
    Signed-off-by: sahithdev9 <sahith.dev9@gmail.com>

--- complete/scripts/start.sh
+++ complete/scripts/start.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo "===== Starting Application ====="
+
+APP_DIR=/home/ec2-user/user-service
+
+cd $APP_DIR
+
+nohup java -jar app.jar > application.log 2>&1 &
+
+echo "Application started successfully."
+
+echo "===== Start Completed ====="
