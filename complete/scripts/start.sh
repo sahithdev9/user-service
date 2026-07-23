83c77a762a8630c330764edb0cd3df6ea89d673d
sahithdev9 <sahith.dev9@gmail.com>
2026-07-23 13:35:23 +0530

    Update start.sh
    Signed-off-by: sahithdev9 <sahith.dev9@gmail.com>

--- complete/scripts/start.sh
+++ complete/scripts/start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
+echo "===== Starting Application ====="
+
 APP_DIR=/home/ec2-user/user-service
 
 cd "$APP_DIR"
 
-JAR=$(ls *.jar | head -1)
+pkill -f "java -jar" || true
+
+JAR=$(ls rest-service-complete-*.jar | head -1)
 
 nohup java -jar "$JAR" > application.log 2>&1 &
+
+sleep 10
+
+echo "Application started successfully."
+
+echo "===== Start Completed ====="
